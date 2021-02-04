5111a1a7750de868d945a9e9e021f8503361c449
handadmin <xiaoyu.zhao@hand-china.com>
2021-02-04 13:53:01 +0800

    [AUTO] 1.4.8.RELEASE

--- src/main/java/org/hzero/swagger/app/impl/SwaggerServiceImpl.java
+++ src/main/java/org/hzero/swagger/app/impl/SwaggerServiceImpl.java
@@ -1,10 +1,15 @@
 package org.hzero.swagger.app.impl;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 import org.apache.commons.collections.keyvalue.MultiKey;
 import org.apache.commons.collections.map.MultiKeyMap;
+import org.hzero.common.HZeroService;
+import org.hzero.core.redis.RedisHelper;
+import org.hzero.swagger.app.SwaggerService;
+import org.hzero.swagger.config.SwaggerProperties;
+import org.hzero.swagger.domain.entity.ServiceRoute;
+import org.hzero.swagger.domain.entity.Swagger;
+import org.hzero.swagger.domain.repository.ServiceRouteRepository;
+import org.hzero.swagger.domain.repository.SwaggerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.ApiKeyVehicle;
@@ -12,12 +17,8 @@ import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.UiConfiguration;
 
-import org.hzero.swagger.app.SwaggerService;
-import org.hzero.swagger.config.SwaggerProperties;
-import org.hzero.swagger.domain.entity.ServiceRoute;
-import org.hzero.swagger.domain.entity.Swagger;
-import org.hzero.swagger.domain.repository.ServiceRouteRepository;
-import org.hzero.swagger.domain.repository.SwaggerRepository;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class SwaggerServiceImpl implements SwaggerService {
@@ -28,6 +29,8 @@ public class SwaggerServiceImpl implements SwaggerService {
     private ServiceRouteRepository serviceRouteRepository;
     @Autowired
     private SwaggerRepository swaggerRepository;
+    @Autowired
+    private RedisHelper redisHelper;
 
     @Override
     public List<SwaggerResource> getSwaggerResource() {
@@ -82,5 +85,8 @@ public class SwaggerServiceImpl implements SwaggerService {
         inert.setValue(json);
 
         swaggerRepository.insert(inert);
+        // 设置服务swagger文档刷新时间
+        this.redisHelper.hshPut(String.format("%s:swagger-refresh:service:%s", HZeroService.Swagger.CODE, serviceName),
+                version, String.valueOf(System.currentTimeMillis()));
     }
 }
